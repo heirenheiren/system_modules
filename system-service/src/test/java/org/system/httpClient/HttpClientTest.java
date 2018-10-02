861438e551cc551ebd705344b8be705d073a5977
heirenheiren <853450031@qq.com>
2018-10-02 12:20:15 +0800

    HttpClient例子

---
+++ system-service/src/test/java/org/system/httpClient/HttpClientTest.java
@@ -0,0 +1,94 @@
+package org.system.httpClient;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+public class HttpClientTest implements HttpClient
+{
+
+	@Override
+	public HttpParams getParams()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ClientConnectionManager getConnectionManager()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpResponse execute(HttpUriRequest request) throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpResponse execute(HttpUriRequest request, HttpContext context) throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpResponse execute(HttpHost target, HttpRequest request) throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HttpResponse execute(HttpHost target, HttpRequest request, HttpContext context)
+			throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler)
+			throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler, HttpContext context)
+			throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler)
+			throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler,
+			HttpContext context) throws IOException, ClientProtocolException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
